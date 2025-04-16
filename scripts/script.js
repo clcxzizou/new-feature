10a8191771758a4d812554cedc3809b24ab990ee
zizou.js <zizouonfreelance@gmail.com>
2025-04-16 21:40:23 +0100

    login page v2

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,12 @@ const navBar = document.querySelector('.nav-bar')
 
 document.addEventListener('scroll',(scroll)=>{
     // console.log(scroll);
-    if(document.documentElement.scrollTop>= 75){
-logoTxt.style.transform = "translateX(-200px)";
-navBar.style.position = "fixed"
+    if(document.documentElement.scrollTop>= 100){
+setTimeout(()=>{
+    logoTxt.style.transform = "translateX(-200px)";
+    navBar.style.position = "fixed"
+    navBar.style.boxshadow ="-2px -9px 20px #6f6a6a;"
+},200)
     }
     else{
         logoTxt.style.transform = "translateX(200px)"
